4cf8ff406e6582f94c709c0f799c97988d7c3847
TomLBO <cnsdbo@163.com>
2015-09-09 00:20:11 +0800

    test222

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -16,16 +16,7 @@ import com.coolweather.app.model.S_City;
 
 public class Utility {
 	private static final String TAG = "Utility";
-	
-	static{
-		City city = new City();
-		City city = new City();City city = new City();
-		City city = new City();
-		Log.i("", "这里是测试代码,测试git提交");Log.i("", "这里是测试代码,测试git提交");Log.i("", "这里是测试代码,测试git提交");
-		Log.i("", "这里是测试代码,测试git提交");
-		Log.i("", "这里是测试代码,测试git提交2");
-		Log.i("", "这里是测试代码,测试git提交3");
-	}
+
 	/**
 	 * 解析和处理服务器返回的省级数据
 	 * @param coolWeatherDB 数据库操作类	
