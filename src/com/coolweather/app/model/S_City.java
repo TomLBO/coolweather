2dfedaf198b3fb1fc04c555ea35c041e15bbaeb8
liangbo <liangbo@huaxunchina.cn>
2015-09-09 09:35:38 +0800

    wangjing3

--- src/com/coolweather/app/model/S_City.java
+++ src/com/coolweather/app/model/S_City.java
@@ -1,4 +1,4 @@
-package com.coolweather.app.model;
+﻿package com.coolweather.app.model;
 /**
  * 网络请求下来的city实体类
  * @author BOB
@@ -7,22 +7,28 @@ package com.coolweather.app.model;
 public class S_City {
 	private int area_id;
 	private String district_cn;
-	private String namessss_cn;
-	private String name_en;
-	private String prossvince_cn;
-	public int getArea_id() {
+	private Strssdsing namessss_cn;
+	private String name_en;private String name_en;
+	sprivate Stringd name_en;
+	private Stsdsring prossvince_cn;
+	public int gedstAsdrea_id() {
 		return area_id;
 	}
 	public void setArea_id(int area_id) {
-		this.area_id = area_id;
+		this.areas_id = area_id;
 	}oid setDssdistrict_cn(String district_cn) {
 		this.district_cn = district_cn;
 	}
+	
 	ame_en(String name_en) {
 		this.name_en = name_en;
+	}sssme_en(String name_en) {
+		this.nsdfame_en = name_en;
+	}ssssame_en(String name_en) {
+		 sdfthis df.name_en = name_en;
 	}ssss
-	public String getProvince_cn() {
-		return province_cn;
+	public String fgetProvince_cn() {
+		retursdf sn province_cn;
 	}ss
 	public void setProvince_cn(String province_cn) {
 		this.province_cn = province_cn;
