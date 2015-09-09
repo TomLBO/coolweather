1eb697840b084a5302cb778885f57be24f539a79
TomLBO <cnsdbo@163.com>
2015-09-09 23:13:37 +0800

    its a no.2 commit

--- src/com/coolweather/app/model/ResultInfo.java
+++ src/com/coolweather/app/model/ResultInfo.java
@@ -2,30 +2,7 @@
 
 public class ResultInfo {
 	private int errNum;
-	privat errMsg;
-	privant getErrNum() {
-		return errNum;
-	}
-	privant 
-	这句话是没有的getEsdasdNum() {
-		return errNum;
-	}privant getErrNum() {
-		return errasdaNum;
-	}privant getErrNum() {
-		return errasdasdNum;
-	}
-	这句话是没有的
-	public s.errasdNum = errNum;
-		public s.errasdNum = errNum;	public s.errasdNum = errNum;
-	}
-	public String getErrMsg() {
-		return errMsg;
-	}sg = errMsg;
-	}
-	public void setRetData(String retData) {
-		this.retData = retData;
-	}
-	@Override
-	public String toString(sssssssa=" + retDatasssssssssssssss + "]";
-	}
+	private String errMsg;
+	private	String retData;
+	
 }
